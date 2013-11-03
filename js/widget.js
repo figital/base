70b22e1e1d4c30d3e478f75a00a602e8f0fcf160
figital <scott@figital.com>
2013-11-03 08:39:38 -0500

    widget stuff

---
+++ js/widget.js
@@ -0,0 +1,32 @@
+/* TargetAnchorInterceptor(shazbot); */
+function intercept(anchor) {
+
+	var target = anchor.target;
+	var href = anchor.href;
+	
+	// console.log(anchor);
+	
+	if (target == "_alert") {
+
+		$.get(href, function( data ) {
+			console.log(JSON.stringify(data));
+			//alert(JSON.stringify(data));
+		});
+
+
+	} else if (target == "_widget") {
+
+
+	} else if (target == "_blank") {
+
+		window.open(href, target);
+	
+	} else {
+
+		alert('TARGET: "' + target + '" does not exist');
+
+	}
+
+	return false;
+	
+}
